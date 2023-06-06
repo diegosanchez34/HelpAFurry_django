944b3dd782ec97d5aa70b0eaedef48ed6c25b9f2
BurritoLopr <shadowcb@gmail.com>
2023-06-06 13:29:43 -0400

    urls views

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path('contactanos',views.contactanos,name='contactanos'),
+    path('crearusuario',views.crearusuario,name='crearusuario'),
     path('gatos',views.gatos,name='gatos'),
+    path('login',views.login,name='login'),
+    path('nosotros',views.nosotros,name='nosotros'),
+    path('perros',views.perros,name='perros'),
+    path('singin',views.singin,name='singin'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- home/views.py
+++ home/views.py
@@ -6,6 +6,30 @@ def index(request):
     context={}
     return render(request, 'home/index.html', context)
 
+def contactanos(request):
+    context={}
+    return render(request,'home/contactanos.html',context)
+
+def crearusuario(request):
+    context={}
+    return render(request,'home/crearusuario.html',context)
+
 def gatos(request):
     context={}
     return render(request,'home/gatos.html',context)
+
+def login(request):
+    context={}
+    return render(request,'home/login.html',context)
+
+def nosotros(request):
+    context={}
+    return render(request,'home/nosotros.html',context)
+
+def perros(request):
+    context={}
+    return render(request,'home/perros.html',context)
+
+def singin(request):
+    context={}
+    return render(request,'home/singin.html',context)
