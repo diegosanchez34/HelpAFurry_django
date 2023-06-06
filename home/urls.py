d7c59b85e6d8f389bf5e7cbd0c5524b2a5841f36
BurritoLopr <shadowcb@gmail.com>
2023-06-06 12:56:11 -0400

    template llenito

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path('gatos',views.gatos,name='gatos'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'home/index.html', context)
+
+def gatos(request):
+    context={}
+    return render(request,'home/gatos.html',context)
